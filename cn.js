1f2d709980bf210dd82270d80f1823ab90ec4155
nextsigner <nextsigner@gmail.com>
2020-05-31 16:46:07 -0300

    se corrige spawn mail 18

--- cn.js
+++ cn.js
@@ -17,7 +17,7 @@ module.exports=function(app){
                 +'d8: '+v8+'\n'
                 +'d9: '+v9+'\n'
                 +''
-        cpEMail = spawnEMail('sh', ['sendEmail.sh', '"Mercurio - Nueva Carta '+sd+'"',''+d+'', 'qtpizarro@gmail.com']);
+        cpEMail = spawnEMail('sh', ['sendEmail.sh', ''+d+'', '"Mercurio - Nueva Carta '+sd+'"', 'qtpizarro@gmail.com']);
         cpEMail.on("exit", function(data) {
             console.log('Mail terminado y enviado');
         });
