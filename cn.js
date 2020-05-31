f1add725943433fb7e239cbc1c5bfb104b7fe23b
nextsigner <nextsigner@gmail.com>
2020-05-31 16:43:39 -0300

    se corrige spawn mail 17

--- cn.js
+++ cn.js
@@ -8,14 +8,14 @@ module.exports=function(app){
         let d0=new Date(Date.now())
         let sd=''+d0.getHours()+':'+d0.getMinutes()+':'+d0.getSeconds()
         let d='d1: '+v1
-                +'d2: '+v2+'<br />'
-                +'d3: '+v3+'<br />'
-                +'d4: '+v4+'<br />'
-                +'d5: '+v5+'<br />'
-                +'d6: '+v6+'<br />'
-                +'d7: '+v7+'<br />'
-                +'d8: '+v8+'<br />'
-                +'d9: '+v9+'<br />'
+                +'d2: '+v2+'\n'
+                +'d3: '+v3+'\n'
+                +'d4: '+v4+'\n'
+                +'d5: '+v5+'\n'
+                +'d6: '+v6+'\n'
+                +'d7: '+v7+'\n'
+                +'d8: '+v8+'\n'
+                +'d9: '+v9+'\n'
                 +''
         cpEMail = spawnEMail('sh', ['sendEmail.sh', '"Mercurio - Nueva Carta '+sd+'"',''+d+'', 'qtpizarro@gmail.com']);
         cpEMail.on("exit", function(data) {
