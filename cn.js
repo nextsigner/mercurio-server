45dbeed021a01dadcc6c00eb71de39383745eae6
nextsigner <nextsigner@gmail.com>
2020-05-31 15:55:47 -0300

    se corrige spawn mail 4

--- cn.js
+++ cn.js
@@ -13,8 +13,8 @@ module.exports=function(app){
                 +'d8: '+v8+'<br />'
                 +'d9: '+v9+'<br />'
                 +'</body></html>'
-        cp = spawn('mail', ['-s', 'Mercurio - Nueva Carta', 'nextsigner@gmail.com', '<<<', '"'+d+'"']);
-        cp.stdout.on("exit", function(data) {
+        cp = spawn('mail', ['-s', 'Mercurio - Nueva Carta', 'nextsigner@gmail.com', '<<<', '"Hola"']);
+        cp.on("exit", function(data) {
             console.log('Mail terminado y enviado');
         });
         cp.stdout.on("data", function(data) {
