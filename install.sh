f763a883f8d552aa380eef9dad089a525f3d2bf7
nextsigner <nextsigner@gmail.com>
2020-05-31 00:11:38 -0300

    se edita install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 !#/bin/bash
+cd /root
+sudo mkdir mercurio-server
+cd mercurio-server
+sudo mkdir files
+sudo chmod -R 755 files
 
-sudo apt install xfce4 xfce4-goodies gnome-icon-theme tightvncserver
+#Instlando aplicaciones necesarias
+sudo apt install git
+git clone https://github.com/nextsigner/zodiacserver.git
+
+#instalando zodiacserver
+
+
+#Instalando entorno de escritorio y aplicaciones necesarias para zodiacserver
+sudo apt install xfce4 xfce4-goodies gnome-icon-theme tightvncserver qt5declarative qtcreators
 
 vncserver
