ca670a687570c752c83f0a8fd749406f17a9c4c6
nextsigner <nextsigner@gmail.com>
2020-06-16 20:23:41 -0300

    se agrega script mercurio

--- install.sh
+++ install.sh
@@ -13,10 +13,15 @@ git clone https://github.com/nextsigner/zodiacserver.git
 
 
 #Instalando entorno de escritorio y aplicaciones necesarias para zodiacserver
-sudo apt install xfce4 xfce4-goodies gnome-icon-theme tightvncserver qt5declarative qtcreators
+sudo apt install xfce4 xfce4-goodies gnome-icon-theme tightvncserver nodejs qt5declarative qtcreators
 
 echo "install.sh copiando archivo /etc/init.d/vncserver..."
 sudo cp vncserver /etc/init.d/vncserver
 sudo chmod +x /etc/init.d/vncserver
 sudo update-rc.d vncserver defaults
+
+echo "install.sh copiando archivo /etc/init.d/mercurio..."
+sudo cp vncserver /etc/init.d/mercurio
+sudo chmod +x /etc/init.d/mercurio
+sudo update-rc.d mercurio defaults
 
