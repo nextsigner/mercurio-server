7105502739dd07a2a5a483d4d7fd4970f7c23fa0
nextsigner <nextsigner@gmail.com>
2020-06-16 20:09:17 -0300

    se edita install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@ git clone https://github.com/nextsigner/zodiacserver.git
 #Instalando entorno de escritorio y aplicaciones necesarias para zodiacserver
 sudo apt install xfce4 xfce4-goodies gnome-icon-theme tightvncserver qt5declarative qtcreators
 
-sudo cp vncserver.sh /etc/init.d/vncserver.sh
-sudo chmod +x /etc/init.d/vncserver.sh
-sudo update-rc.d vncserver.sh defaults
+echo "install.sh copiando archivo /etc/init.d/vncserver..."
+sudo cp vncserver /etc/init.d/vncserver
+sudo chmod +x /etc/init.d/vncserver
+sudo update-rc.d vncserver defaults
 
