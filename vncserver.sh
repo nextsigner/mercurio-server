ef0b053e4f94234056311edf8a70cce0d8f2ddf6
nextsigner <nextsigner@gmail.com>
2020-06-16 18:07:42 -0300

    se edita vncserver.sh 3

--- vncserver.sh
+++ vncserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ### BEGIN INIT INFO
 # Provides:          vncserver
-# Required-Start:    $syslog
-# Required-Stop:     $syslog
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Mercurio Server
-# Description: Iniciar Vnc Server para Mercurio Server
+# Short-Description: Start daemon at boot time
+# Description:       Enable vncserver for mercurio-server service provided by daemon.
 ### END INIT INFO
 vncserver :1 -geometry 1920x1080
