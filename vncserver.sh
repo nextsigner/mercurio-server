0b981a36f113ab2cd38c2968bb50c38ead68af3f
nextsigner <nextsigner@gmail.com>
2020-06-16 17:53:53 -0300

    se edita vncserver.sh 2

--- vncserver.sh
+++ vncserver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ### BEGIN INIT INFO
-# Provides:          vncserver-mercurio
+# Provides:          mercurio
 # Required-Start:    $syslog
 # Required-Stop:     $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Iniciar VncServer para Mercurio Server
-# Description: 
+# Short-Description: Iniciar forever de Mercurio Server
+# Description:
 #
 ### END INIT INFO
 vncserver :1 -geometry 1920x1080
