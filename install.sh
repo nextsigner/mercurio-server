09113d94620872976cde6336b1876602b66d66d2
nextsigner <nextsigner@gmail.com>
2020-06-17 12:40:00 -0300

    se edita install.sh 4

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs 
 sudo apt-get install -y npm
 
+npm install
+
 echo "install.sh copiando archivo /etc/init.d/vncserver..."
 sudo cp xstartup ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
