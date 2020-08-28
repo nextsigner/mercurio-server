23165b8059ec4aba02a21da5b30e1126287bc988
nextsigner <nextsigner@gmail.com>
2020-08-28 00:11:16 -0300

    se agrega codigo de hades 2

--- cn.js
+++ cn.js
@@ -1,56 +1,10 @@
 module.exports=function(app){
 
+    var jsonHades=''
     //-->Hades
-    const {AstrologyService, AspectService,
-        EphemerisJSONRepository, OrbJSONRepository,
-        TrigonometricUtilities,HouseSystemFactory,
-        TimeConversions, WorldTimezoneRepository,
-        ZodiacFactory, GeodeticLocation, HouseSystemType,
-        RetrogradesService} = require("@goldenius/hades-js");
-    const moment = require('moment-timezone');
-
-    let timeConversions = new TimeConversions();
-    let retrogradesService = new RetrogradesService();
-    let ephemerisJSONRepository = new EphemerisJSONRepository(timeConversions,retrogradesService);
-    let worldTimezoneRepository = new WorldTimezoneRepository();
-    let orbRepository = new OrbJSONRepository();
-    let aspectService = new AspectService(orbRepository);
-    let trigonometricUtilities = new TrigonometricUtilities();
-    let zodiacFactory = new ZodiacFactory();
-    let houseSystemFactory = new HouseSystemFactory(trigonometricUtilities,zodiacFactory);
-
-
-    let astrologyService = new AstrologyService(ephemerisJSONRepository,
-                                                timeConversions,
-                                                worldTimezoneRepository,
-                                                aspectService,
-                                                houseSystemFactory);
-
-    let location = new GeodeticLocation(''+lon,''+lat);
-    let date = moment(''+anio+'-'+mes+'-'+dia+' '+hora+':'+minutos+':00');
-    let timezone = 'America/Argentina/Buenos_Aires';
-
-    let celestialBodiesAndTime = testPlanetCalculation();
-    let calculatedAspects = testAspects(celestialBodiesAndTime.CelestialBodies);
-    let calculatedHouses = testHouseCalculation(HouseSystemType.Placidus);
-
-    console.log(JSON.stringify(celestialBodiesAndTime));
-    console.log(JSON.stringify(calculatedAspects));
-    console.log(JSON.stringify(calculatedHouses));
-
-    let fullData=JSON.stringify(celestialBodiesAndTime)
-    fullData+=JSON.stringify(calculatedAspects)
-    fullData+=JSON.stringify(calculatedHouses)
+    // write to a new file named 2pac.txt
     const fs = require('fs');
 
-    // write to a new file named 2pac.txt
-    fs.writeFile('/home/nextsigner/salida.txt', fullData, (err) => {
-                     // throws an error, you could also catch it here
-                     if (err) throw err;
-
-                     // success case, the file was saved
-                     console.log('FullData saved!');
-                 });
 
     function testPlanetCalculation()
     {
@@ -114,39 +68,93 @@ module.exports=function(app){
         console.log('Get new cn: '+v1+' '+v2+' '+v3+' '+v4+' '+v5+' '+v6+' '+v7+' '+v8+' '+v9+' '+v10+' '+fn+' '+ms)
         let o={'file':''+ms+'_'+v1}
 
-        //Hades
+        //-->Hades
         var dia = v3
         var mes = v2
         var anio = v1
         var hora = v4
         var minutos = v5
-        var lat = parseFloat(v8)
-        var lon = parseFloat(v9)
+        var lat = v8//parseFloat(v8)
+        var lon = v9//parseFloat(v9)
         console.log("Calculando Carta Natal ..."+dia+"/"+mes+"/"+anio+" "+hora+":"+minutos+"hs lat:"+lat+" lon:"+lon)
 
-        cp = spawn('/root/mercurio-server/zodiacserver/bin/zodiac_server', [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, fn, ms, '10', '/root/mercurio-server/files/'+ms+'_'+v1+'.png', '5120x2880']);
-        cp.stdout.on("data", function(data) {
-            if(data.toString().trim().indexOf('AppSettings: saved to')>=0){
-                console.log('Sistema Mercurio: '+data.toString());
-            }
-            if(data.toString().trim().indexOf('Saving json file')>=0){
-                console.log('Sistema Mercurio: '+data.toString());
-                res.status(200).send(o)
-            }
-        });
-        cp.stderr.on("data", function(data) {
-            //console.error(data.toString());
-            if(data.toString().trim().indexOf('AppSettings: saved to')>=0){
-                //console.log('Response code 1')
-                //res.status(200).send(o)
-            }
-            if(data.toString().trim().indexOf('Saving json file')>=0){
-                //Zodiac devuelve salida por qDebug() como error.
-                console.log('Sistema Mercurio: '+data.toString());
-                res.status(200).send(o)
-            }
-        });
-        setAndSendEmail(v1, v2, v3, v4, v5, v6, v7, v8, v9)
+        const {AstrologyService, AspectService,
+            EphemerisJSONRepository, OrbJSONRepository,
+            TrigonometricUtilities,HouseSystemFactory,
+            TimeConversions, WorldTimezoneRepository,
+            ZodiacFactory, GeodeticLocation, HouseSystemType,
+            RetrogradesService} = require("@goldenius/hades-js");
+        const moment = require('moment-timezone');
+
+        let timeConversions = new TimeConversions();
+        let retrogradesService = new RetrogradesService();
+        let ephemerisJSONRepository = new EphemerisJSONRepository(timeConversions,retrogradesService);
+        let worldTimezoneRepository = new WorldTimezoneRepository();
+        let orbRepository = new OrbJSONRepository();
+        let aspectService = new AspectService(orbRepository);
+        let trigonometricUtilities = new TrigonometricUtilities();
+        let zodiacFactory = new ZodiacFactory();
+        let houseSystemFactory = new HouseSystemFactory(trigonometricUtilities,zodiacFactory);
+
+
+        let astrologyService = new AstrologyService(ephemerisJSONRepository,
+                                                    timeConversions,
+                                                    worldTimezoneRepository,
+                                                    aspectService,
+                                                    houseSystemFactory);
+
+        let location = new GeodeticLocation(''+lon,''+lat);
+        let dateH = moment(''+anio+'-'+mes+'-'+dia+' '+hora+':'+minutos+':00');
+        let timezone = 'America/Argentina/Buenos_Aires';
+
+        let celestialBodiesAndTime = testPlanetCalculation();
+        let calculatedAspects = testAspects(celestialBodiesAndTime.CelestialBodies);
+        let calculatedHouses = testHouseCalculation(HouseSystemType.Placidus);
+
+        console.log(JSON.stringify(celestialBodiesAndTime));
+        console.log(JSON.stringify(calculatedAspects));
+        console.log(JSON.stringify(calculatedHouses));
+
+        let dtf=new Date(Date.now())
+        jsonHades='/tmp/'+dtf.getTime()+'.json'
+
+        let fullData=JSON.stringify(celestialBodiesAndTime)
+        fullData+=JSON.stringify(calculatedAspects)
+        fullData+=JSON.stringify(calculatedHouses)
+        fs.writeFile(jsonHades, fullData, (err) => {
+                         // throws an error, you could also catch it here
+                         if (err) throw err;
+
+                         // success case, the file was saved
+                         console.log('jsonHades: '+jsonHades);
+
+                         //-->Spawn zodiacserver
+                         cp = spawn('/root/mercurio-server/zodiacserver/bin/zodiac_server', [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, fn, ms, '10', '/root/mercurio-server/files/'+ms+'_'+v1+'.png', '5120x2880']);
+                         cp.stdout.on("data", function(data) {
+                             if(data.toString().trim().indexOf('AppSettings: saved to')>=0){
+                                 console.log('Sistema Mercurio: '+data.toString());
+                             }
+                             if(data.toString().trim().indexOf('Saving json file')>=0){
+                                 console.log('Sistema Mercurio: '+data.toString());
+                                 res.status(200).send(o)
+                             }
+                         });
+                         cp.stderr.on("data", function(data) {
+                             //console.error(data.toString());
+                             if(data.toString().trim().indexOf('AppSettings: saved to')>=0){
+                                 //console.log('Response code 1')
+                                 //res.status(200).send(o)
+                             }
+                             if(data.toString().trim().indexOf('Saving json file')>=0){
+                                 //Zodiac devuelve salida por qDebug() como error.
+                                 console.log('Sistema Mercurio: '+data.toString());
+                                 res.status(200).send(o)
+                             }
+                         });
+                         setAndSendEmail(v1, v2, v3, v4, v5, v6, v7, v8, v9)
+                         //<--Spawn zodiacserver
+                     });
+        //<---Hades
     }
 
 
